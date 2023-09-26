7a4285f8f5d458ec96e2298c4d2f6fdc49baa561
Mwangi <mwangi8kevin@gmail.com>
2023-09-26 05:11:31 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,18 +1,12 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  function conditionalBlock () {
-    let task = false;
-    let task2 = true;
-
-    if (trueOrFalse) {
-      task = true;
-      task2 = false;
-    }
-
-    return [task, task2];
+  if (trueOrFalse) {
+    const innerTask = true;
+    const innerTask2 = false;
+    return [innerTask, innerTask2];
   }
 
-  return conditionalBlock();
+  return [task, task2];
 }
