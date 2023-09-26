39f05cc056ab0d4f3a0588aa2743456f79418486
mwangiii <mwangi8kevin@gmail.com>
2023-09-26 04:15:00 +0300

    debugging second file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock (trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const innertask = true;
-    const innertask2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
