1dbd16b1a3d86a151f7e8f935b2e33022b615ea4
mwangiii <mwangi8kevin@gmail.com>
2023-09-28 03:04:26 +0300

    code 7-getBudgetObject.js

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income: income,
+      gdp: gdp,
+      capita: capita,
+    };
+  
+    return {budget};
+  }
