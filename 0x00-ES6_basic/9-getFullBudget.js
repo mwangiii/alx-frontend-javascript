48daa1b3b2a635bed1b8dfebbcb2dcda9e120090
mwangiii <mwangi8kevin@gmail.com>
2023-10-02 11:48:09 +0300

    debuging number 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,15 +1,15 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
-export default function getFullBudgetObject (income, gdp, capita) {
+export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) {
+    getIncomeInDollars: function(income) {
       return `$${income}`;
     },
-    getIncomeInEuros: function (income) {
+    getIncomeInEuros: function(income) {
       return `${income} euros`;
-    }
+    },
   };
 
   return fullBudget;
