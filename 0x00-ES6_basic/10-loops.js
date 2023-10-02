ae61e76a80dc9a9a0471082f16e6f59d58665666
mwangiii <mwangi8kevin@gmail.com>
2023-10-02 13:12:23 +0300

    remove ;

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
     const newArray = [];
     for (const value of array) {
       newArray.push(appendString + value);
-    };
+    }
 
     return newArray;
   }
