db00141be882886fdee82528e94f51145a812a12
mwangiii <mwangi8kevin@gmail.com>
2023-10-02 13:05:18 +0300

    adding a ; on file 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
     let newArray = [];
     for (let value of array) {
       newArray.push(appendString + value);
-    }
+    };
 
     return newArray;
   }
