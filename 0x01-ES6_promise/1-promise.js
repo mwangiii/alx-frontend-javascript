9da0cde34778d7b335d0f804a6116ce288c5d1ba
mwangiii <mwangi8kevin@gmail.com>
2023-10-04 13:59:20 +0300

    fixing file 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  if (true) {
-    return Promise.resolve('staus; 200, body: "Success"');
-  } else {
-    return Promise.reject(Error('The fake API is not working currently'));
-  }
+  return Promise((resolve, reject) => {
+    if (success){
+      resolve({ status: 200, body: 'Success'});
+    } else {
+      reject(Error('The fake APi is not working currently'));
+    }
+  })
 }
