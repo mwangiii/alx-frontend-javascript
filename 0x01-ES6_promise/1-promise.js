5eaccf493d86b7e68971f061336dddaa52dcd106
mwangiii <mwangi8kevin@gmail.com>
2023-10-04 14:12:53 +0300

    capital I on API

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success){
-      resolve({ status: 200, body: 'Success'});
+    if (success) {
+      resolve({ status: 200, body: 'Success'} );
     } else {
-      reject(new Error('The fake APi is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
-  })
+  });
 }
