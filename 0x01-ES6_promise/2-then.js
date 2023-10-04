bf821a32862066a96ebf8ac7ea21950f59c79502
mwangiii <mwangi8kevin@gmail.com>
2023-10-04 15:53:23 +0300

    adding a then snippet

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,16 @@ export default function handleResponseFromAPI (promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
       resolve({ status: 200, body: 'success' });
-      console.log('Got a response from the API');
     } else {
       reject(new Error());
       console.log('Got a response from the API');
     }
+  })
+ .then((result) => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' }
+  })
+  .catch((error) => {
+    return new Error();
   });
 }
