aae0f2d8e4b71df598fbd20cd4a9ac29cd7ae0eb
mwangiii <mwangi8kevin@gmail.com>
2023-10-04 13:29:37 +0300

    try running using true

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,7 @@
 export default function getFullResponseFromAPI(success) {
-  const cond = true;
-  if (cond) {
+  if (true) {
     return Promise.resolve('staus; 200, body: "Success"');
-  }else {
+  } else {
     return Promise.reject(Error('The fake API is not working currently'));
   }
 }
