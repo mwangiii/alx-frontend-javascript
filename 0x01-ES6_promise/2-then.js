03daa08b465862ee1df52fb33a701f59a13173df
mwangiii <mwangi8kevin@gmail.com>
2023-10-04 16:30:05 +0300

    fixed file 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,12 @@
 export default function handleResponseFromAPI (promise) {
   return promise
     .then((result) => {
-      console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
     .catch((Error) => {
-      console.error('Got an error from the API');
       return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
