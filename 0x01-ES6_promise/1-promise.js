bd8c28c6f503f13f70774594871c819b8d13f0cc
mwangiii <mwangi8kevin@gmail.com>
2023-10-04 14:09:18 +0300

    adding new before promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 export default function getFullResponseFromAPI(success) {
-  return Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success){
       resolve({ status: 200, body: 'Success'});
     } else {
