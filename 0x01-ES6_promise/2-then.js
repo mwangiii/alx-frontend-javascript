dea6631be86d8ee6646f631c8ad02b1f89622cf8
mwangiii <mwangi8kevin@gmail.com>
2023-10-06 11:23:21 +0300

    file 2 and making file 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((result = { status: 200, body: 'success' }) => result)
+    .then((data = { status: 200, body: 'success' }) => data)
     .catch((error = new Error()) => error)
     .finally(console.log('Got a response from the API'));
 }
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,20 @@
+import { myFunction } from './2-then.js';
+function handleProfileSignup(){
+    
+}
+
+
+// In this file, import uploadPhoto and createUser from utils.js
+
+// Knowing that the functions in utils.js return promises, use the prototype below to collectively resolve all promises and log body firstName lastName to the console.
+
+
+// In the event of an error, log Signup system offline to the console
+
+// export default function handleResponseFromAPI(promise) {
+//     return promise
+//       .then((result = { status: 200, body: 'success' }) => result)
+//       .catch((error = new Error()) => error)
+//       .finally(console.log('Got a response from the API'));
+//   }
+  
