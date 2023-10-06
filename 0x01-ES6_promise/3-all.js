47d7a9a3d953f808c972eb4ded01ad5d16507ef4
mwangiii <mwangi8kevin@gmail.com>
2023-10-06 12:43:43 +0300

    file 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,10 @@
 import { uploadPhoto,createUser } from './2-then.js';
 export default function handleProfileSign() {
-  return promise 
-    .then ((result = { status: 200, body: { firstName, lastName }}) => result)
-    .catch((error = new Error('Signup system offline')) => error)
+  return new Promise ((resolve,reject) => {
+    if (promise){
+      resolve({body:{firstName,lastName}});
+    } else {
+      reject(new Error('Signup system offline'))
+    }
+  })
 }
