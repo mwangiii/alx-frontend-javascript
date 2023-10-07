07dd6645e898fc88820bb5e873168c353332f15a
mwangiii <mwangi8kevin@gmail.com>
2023-10-07 16:34:19 +0300

    js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  return Promise.allSettled(promises).then((values) => values.map((value) =>
-    ({
-      status: value.status,
-      value: value.status === 'fulfilled' ? value.value : `Error: ${value.reason.message}`,
-    }))
-  );
+  return Promise.allSettled(promises).then((values) => values.map((value) => ({
+    status: value.status,
+    value: value.status === 'fulfilled' ? value.value : `Error: ${value.reason.message}`,
+  })));
 }
