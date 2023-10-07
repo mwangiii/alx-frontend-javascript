e2593e2400239c5eda8661920858cea1e47056a0
mwangiii <mwangi8kevin@gmail.com>
2023-10-07 16:07:39 +0300

    file 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  return Promise.allSettled(promises).then((values) => {
-    return values.map((value) => ({
+  return Promise.allSettled(promises).then((values) => values.map((value) => 
+  ({
       status: value.status,
       value: value.status === 'fulfilled' ? value.value : `Error: ${value.reason.message}`,
-    }));
-  });
+    }))
+  );
 }
