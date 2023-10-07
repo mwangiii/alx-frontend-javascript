784da151b8533e99584317a3a9b897b65ccaac5c
mwangiii <mwangi8kevin@gmail.com>
2023-10-07 14:36:51 +0300

    file 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,12 @@
-import {signUpUser} from './4-user-promise';
-import {uploadPhoto} from './5-photo-reject';
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName){
-  const arr = {
-    status: 'status_of_the_promise',
-    value: 'value or error returned by the Promise',
-  }
-  return Promise.all ([arr])
+export default function handleProfileSignup (firstName, lastName, fileName) {
+  const arr = [
+    {
+      status: 'status_of_the_promise',
+      value: 'value or error returned by the Promise'
+    }
+  ];
+  return Promise.all(arr);
 }
