836e12ad8d5c6fa40614e2179ec019d29fcbd2a3
mwangiii <mwangi8kevin@gmail.com>
2023-10-13 19:00:29 +0300

    commit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import {ClassRoom} from './0-classroom';
+import {ClassRoom} from './0-classroom'
 
 export default function initializeRooms (){
   const roomSize = [19, 20, 34];
   const classrooms = [];
 
-  for (const size of roomSizes) {
+  for (const size of roomSize) {
     classrooms.push(new ClassRoom(size));
   }
 
