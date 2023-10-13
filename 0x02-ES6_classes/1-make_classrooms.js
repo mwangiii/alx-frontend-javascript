79b327a30a09358bbe38e8af288eaff3e98c0e1a
mwangiii <mwangi8kevin@gmail.com>
2023-10-13 17:24:52 +0300

    fixing file 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,5 +2,12 @@ import {ClassRoom} from './0-classroom'
 
 export default function initializeRooms (){
   super(maxStudentsSize);
-  return [].ClassRoom(19, 20, 34)
+  const roomSize = [19, 20, 34];
+  const classRooms = [];
+
+  for (const size of roomSizes) {
+    classrooms.push(new ClassRoom(size));
+  }
+
+  return classrooms;
 }
