6ae38963b18778b40c2fa3868baed4360cbee91e
mwangiii <mwangi8kevin@gmail.com>
2023-10-13 19:05:37 +0300

    fixing the code

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,6 @@
-import {ClassRoom} from './0-classroom'
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms (){
-  const roomSize = [19, 20, 34];
-  const classrooms = [];
-
-  for (const size of roomSize) {
-    classrooms.push(new ClassRoom(size));
-  }
-
-  return classrooms;
+export default function initializeRooms() {
+  const array = [19, 20, 34];
+  return array.map((x) => new ClassRoom(x));
 }
