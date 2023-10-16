4d6d5cf387cdad25e470bc3775bb25b6687f441d
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 17:01:27 +0300

    advanced

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,11 @@
-export default function guardrail(mathFunction){
-  const queue = []
+export default function guardrail(mathFunction) {
+  const queue = [];
   try {
-    const result = mathFunction();
-    queue.push(result);
+    queue.push(mathFunction());
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
