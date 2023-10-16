0ab0d997917dd15bddc3deef53c180ee1f046993
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 18:51:18 +0300

    abbys code

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,12 @@
-export default function createInt8TypedArray() {
-    if (position < 0 || position >= length) {
-      throw new Error('Position outside range');
-    }
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    const buffer = new ArrayBuffer(length);  
-    const int8Array = new Int8Array(buffer);
-  
-    int8Array[position] = value;  
-    return buffer;
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  view.setInt8(position, value);
+
+  return view;
 }
