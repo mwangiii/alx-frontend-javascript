be2001b7bf759b4ce38f0734aeb244eb7f7aedd9
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 12:26:07 +0300

    using class currency

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,6 +2,12 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw TypeError('currency must be a Currency object');
+    }
     this._amount = amount;
     this._currency = currency;
   }
@@ -19,7 +25,7 @@ export default class Pricing {
   }
 
   set currency(newCurrency) {
-    this._currency = newCurrency; 
+    this._currency = newCurrency;
   }
 
   displayFullPrice() {
