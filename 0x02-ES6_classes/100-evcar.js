879f79eba3b342b17657ec6b884c80f5c5e7a098
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 16:15:46 +0300

    advanced tasks

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,12 @@
-import Car from '10-car';
+import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range){
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
-  cloneCar(){
+
+  cloneCar() {
     return new Car();
   }
 }
