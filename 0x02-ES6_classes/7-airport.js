15f871d6aa026c2618eb7cd4cc4829fc95d0c9a8
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 14:20:22 +0300

    mikes code

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,7 +3,8 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
+
   toString() {
-    return `${this._code}`;
-  }  
+    return `[object ${this._code}]`;
+  }
 }
