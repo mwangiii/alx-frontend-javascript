2e7e134c196407dea45bc9e76677ec74e2be8518
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 15:02:02 +0300

    fixed

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -8,7 +7,7 @@ export class HolbertonClass {
   get year() {
     return this._year;
   }
-  
+
   get location() {
     return this._location;
   }
@@ -17,18 +16,17 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
-  
+
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
-  
+
   get holbertonClass() {
     return this._holbertonClass;
   }
@@ -47,4 +45,4 @@ const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 
-export default listOfStudents; 
+export default listOfStudents;
