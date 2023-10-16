13b4b67c6b3545b3c580e5b92e31eb099c7b9a20
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 15:08:23 +0300

    eslint fix for real this time

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -36,7 +36,6 @@ export class StudentHolberton {
   }
 }
 
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
