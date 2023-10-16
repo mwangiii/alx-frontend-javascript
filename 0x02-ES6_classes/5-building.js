0bf68eb63f9ebf304f88b6055c0fa03c574c1bf0
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 13:41:40 +0300

    adding spaces

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 export default class Building {
   constructor(sqft) {
-    if(typeof sqft !== 'number') {
+    if (typeof sqft !== 'number') {
       throw TypeError('sqft should be a number');
     }
     this._sqft = sqft;
 
-    if(this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
