bffa430ef468fa9c4073661cbe2714343caf705d
mwangiii <mwangi8kevin@gmail.com>
2023-10-16 19:02:37 +0300

    abbys code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,11 @@
 export default function cleanSet(set, startString) {
-  return Array.from(set)
-    .filter(value => value.startsWith(startString))
-    .join('-');
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+
+  const resArray = Array.from(set)
+    .filter((item) => (item !== undefined ? item.startsWith(startString) : ''))
+    .map((item) => (item !== undefined ? item.substring(startString.length) : ''));
+
+  return resArray.join('-');
 }
