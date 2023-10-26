d2192b194d3a76f52bea5c1ffeff3c795486371c
mwangiii <mwangi8kevin@gmail.com>
2023-10-26 09:14:14 +0300

    typescript

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++
@@ -1,7 +0,0 @@
-export default function getStudentsByLocation(students, city) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  const studentsInCity = students.filter((student) => student.location === city);
-  return studentsInCity;
-}
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++
@@ -1,7 +0,0 @@
-export default function getStudentIdsSum(students) {
-  if (!Array.isArray(students)) {
-    return 0;
-  }
-  const sumOfIds = students.reduce((accumulator, student) => accumulator + student.id, 0);
-  return sumOfIds;
-}
